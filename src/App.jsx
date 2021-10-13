890fcc1a3476c75c2d3f9f57097fb179fcad7d76
Eduardo <el_alienx@hotmail.com>
2021-10-13 08:21:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// NPM Packages
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Project files
+import Category from "./pages/Category";
+import Home from "./pages/Home";
+import Vehicle from "./pages/Vehicle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={Category} path="category/:category-name" />
+          <Route component={Vehicle} path="vehicle/:id" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
